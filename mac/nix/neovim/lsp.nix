fb8a375e800f55416544f2da354697a913c09bb0
Helge Rene Urholm <helge@urholm.no>
2025-09-21 19:32:50 +0200

    feat(neovim): add C# support via OmniSharp LSP
    - Add OmniSharp LSP server with recommended settings
    - Configure import completion and Roslyn analyzers
    - Enable automatic format on save
    - C# treesitter support already present

--- mac/nix/neovim/lsp.nix
+++ mac/nix/neovim/lsp.nix
@@ -11,6 +11,17 @@
               AutomaticWorkspaceInit = true;
             };
           };
+          omnisharp = {        # C#
+            enable = true;
+            package = pkgs.omnisharp-roslyn;
+            extraOptions = {
+              enable_import_completion = true;
+              enable_roslyn_analyzers = true;
+              organize_imports_on_format = true;
+              enable_ms_build_load_projects_on_demand = true;
+              sdk_include_prereleases = true;
+            };
+          };
         };
       };
 
