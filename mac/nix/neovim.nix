89ba2a7b4ab74dab0609cea8fd63ce571a833543
Helge Rene Urholm <helge@urholm.no>
2025-09-21 17:22:27 +0200

    refactor(neovim): remove inline keymaps in favor of modular system

--- mac/nix/neovim.nix
+++ mac/nix/neovim.nix
@@ -36,40 +36,7 @@
       #############
       # Keymaps #
       #############
-      keymaps = [
-        # Nix-specific keymaps
-        {
-          key = "<leader>nf";
-          action = ":lua vim.lsp.buf.format()<CR>";
-          mode = "n";
-          options.desc = "Format Nix file";
-        }
-        {
-          key = "<leader>na";
-          action = ":lua vim.lsp.buf.code_action()<CR>";
-          mode = "n";
-          options.desc = "Nix code actions";
-        }
-        {
-          key = "<leader>nr";
-          action = ":!nix run<CR>";
-          mode = "n";
-          options.desc = "Run nix project";
-        }
-        # General keymaps
-        {
-          key = "<C-n>";
-          action = ":Neotree toggle<CR>";
-          mode = "n";
-        }
-        {
-          key = "<leader>e";
-          action = ":Neotree focus<CR>";
-          mode = "n";
-        }
-
-
-      ];
+      keymaps = (import ./neovim/keymaps { inherit pkgs; }).keymaps;
 
       globals.mapleader = " ";
     };
