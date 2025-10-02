66c711d87b581faf5ea791c65fbdad23de529c6c
Helge Rene Urholm <helge@urholm.no>
2025-10-02 21:29:59 +0200

    feat: implement mason keymaps (1 keymap)
    - Add mason.nix with 1 Mason keymap (<leader>cm)
    - Update default.nix to import mason section
    - Tested successfully with sudo switch

--- mac/nix/nvf/keymaps/default.nix
+++ mac/nix/nvf/keymaps/default.nix
@@ -4,6 +4,7 @@ let
   general = import ./general.nix;
   lsp = import ./lsp.nix;
   bufferline = import ./bufferline.nix;
+  mason = import ./mason.nix;
 in
 # Concatenate all keymaps
-  general ++ lsp ++ bufferline
+  general ++ lsp ++ bufferline ++ mason
---
+++ mac/nix/nvf/keymaps/mason.nix
@@ -0,0 +1,11 @@
+# Mason.nvim Keymaps
+# LazyVim keymaps for mason.nvim - 1 keymap
+
+[
+  {
+    mode = ["n"];
+    key = "<leader>cm";
+    action = "<cmd>Mason<cr>";
+    desc = "Mason";
+  }
+]
